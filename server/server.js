511af3e645feb7e94bf84c72c98475347e0063d5
Nicola <nicola.dalmaso@outlook.com>
2017-07-18 16:46:59 +0200

    Added serverside versions fetching

--- server/server.js
+++ server/server.js
@@ -5,28 +5,46 @@ import axios from 'axios';
 import bodyParser from 'body-parser';
 import path from 'path';
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.post('/builds', (req, res) => {
-  console.log('Request:', req.body, req.params);
-  const body = req.body;
-  let { repository } = body;
+const backendPackageJson = require('./package.json');
+const frontendPackageJson = require('../client/package.json');
+const rootPackageJson = require('../package.json');
+
+function fetchBuilds(requestBody, response) {
+  let { repository } = requestBody;
   if (repository) {
     repository = repository.trim();
     fetcher.fetch(repository, client.create(axios))
       .then((builds) => {
         console.log('builds fetched');
-        res.json(builds);
+        response.json(builds);
       })
       .catch((error) => {
         console.error(error);
-        res.status(500).send('Wrong repository or something else');
+        response.status(500).send('Wrong repository or something else');
       });
   } else {
     console.warn('Missing repository info');
-    res.status(500).send('Missing repository info.');
+    response.status(500).send('Missing repository info.');
   }
+}
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.post('/builds', (req, res) => {
+  console.log('Request:', req.body, req.params);
+  fetchBuilds(req.body, res);
+});
+
+app.get('/versions', (req, res) => {
+  console.log('Request for versions');
+  res.send({
+    backendVersion: backendPackageJson.version,
+    frontendVersion: frontendPackageJson.version,
+    rootVersion: rootPackageJson.version,
+    coreVersion: backendPackageJson.dependencies['travis-builds-reporter-core'],
+    utilsVersion: frontendPackageJson.dependencies['travis-builds-reporter-utils'] || 'latest',
+  });
 });
 
 // Express only serves static assets in production
