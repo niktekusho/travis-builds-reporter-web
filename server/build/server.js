337685865b4cd26a68e2888b48f6cdf61c250f56
Nicola <nicola.dalmaso@outlook.com>
2017-07-18 17:46:13 +0200

    Fixed heroku caching frontend files. About page now use serverside versions.

--- server/build/server.js
+++ server/build/server.js
@@ -22,38 +22,72 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
+var _fs = require('fs');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express2.default)();
-app.use(_bodyParser2.default.json());
-app.use((0, _cors2.default)());
-app.post('/builds', function (req, res) {
-  console.log('Request:', req.body, req.params);
-  var body = req.body;
-  var repository = body.repository;
-
-  if (repository) {
-    repository = repository.trim();
-    _travisBuildsReporterCore.fetcher.fetch(repository, _travisBuildsReporterCore.client.create(_axios2.default)).then(function (builds) {
-      console.log('builds fetched');
-      res.json(builds);
-    }).catch(function (error) {
-      console.error(error);
-      res.status(500).send('Wrong repository or something else');
-    });
-  } else {
-    console.warn('Missing repository info');
-    res.status(500).send('Missing repository info.');
-  }
-});
-
-// Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(_express2.default.static(_path2.default.join(__dirname, '../../client/build')));
+function pathFinder(currentDir) {
+  (0, _fs.exists)(currentDir + '/Procfile', function (exists) {
+    if (exists) {
+      return main(currentDir);
+    } else {
+      return pathFinder(_path2.default.join(currentDir, '/', '..'));
+    }
+  });
 }
 
-app.set('port', process.env.PORT || 3001);
+function main(rootProjectDir) {
+  var backendPackageJson = require(rootProjectDir + '/server/package.json');
+  var frontendPackageJson = require(rootProjectDir + '/client/package.json');
+  var rootPackageJson = require(rootProjectDir + '/package.json');
 
-app.listen(app.get('port'), function () {
-  console.log('Listening on ' + app.get('port'));
-});
+  function fetchBuilds(requestBody, response) {
+    var repository = requestBody.repository;
+
+    if (repository) {
+      repository = repository.trim();
+      _travisBuildsReporterCore.fetcher.fetch(repository, _travisBuildsReporterCore.client.create(_axios2.default)).then(function (builds) {
+        console.log('builds fetched');
+        response.json(builds);
+      }).catch(function (error) {
+        console.error(error);
+        response.status(500).send('Wrong repository or something else');
+      });
+    } else {
+      console.warn('Missing repository info');
+      response.status(500).send('Missing repository info.');
+    }
+  }
+
+  var app = (0, _express2.default)();
+  app.use(_bodyParser2.default.json());
+  app.use((0, _cors2.default)());
+  app.post('/builds', function (req, res) {
+    console.log('Request:', req.body, req.params);
+    fetchBuilds(req.body, res);
+  });
+
+  app.get('/versions', function (req, res) {
+    console.log('Request for versions');
+    res.send({
+      backendVersion: backendPackageJson.version,
+      frontendVersion: frontendPackageJson.version,
+      rootVersion: rootPackageJson.version,
+      coreVersion: backendPackageJson.dependencies['travis-builds-reporter-core'],
+      utilsVersion: frontendPackageJson.dependencies['travis-builds-reporter-utils'] || 'latest'
+    });
+  });
+
+  // Express only serves static assets in production
+  if (process.env.NODE_ENV === "production") {
+    app.use(_express2.default.static(_path2.default.join(rootProjectDir, 'client/build')));
+  }
+
+  app.set('port', process.env.PORT || 3001);
+
+  app.listen(app.get('port'), function () {
+    console.log('Listening on ' + app.get('port'));
+  });
+}
+
+pathFinder(__dirname);
--- server/server.js
+++ server/server.js
@@ -4,58 +4,71 @@ import { client, fetcher } from 'travis-builds-reporter-core';
 import axios from 'axios';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { exists } from 'fs';
 
-const backendPackageJson = require('./package.json');
-const frontendPackageJson = require('../client/package.json');
-const rootPackageJson = require('../package.json');
-
-function fetchBuilds(requestBody, response) {
-  let { repository } = requestBody;
-  if (repository) {
-    repository = repository.trim();
-    fetcher.fetch(repository, client.create(axios))
-      .then((builds) => {
-        console.log('builds fetched');
-        response.json(builds);
-      })
-      .catch((error) => {
-        console.error(error);
-        response.status(500).send('Wrong repository or something else');
-      });
-  } else {
-    console.warn('Missing repository info');
-    response.status(500).send('Missing repository info.');
-  }
+function pathFinder(currentDir) {
+  exists(`${currentDir}/Procfile`, (exists) => {
+    if (exists) {
+      return main(currentDir);
+    } else {
+      return pathFinder(path.join(currentDir, '/', '..'));
+    }
+  });
 }
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.post('/builds', (req, res) => {
-  console.log('Request:', req.body, req.params);
-  fetchBuilds(req.body, res);
-});
+function main(rootProjectDir) {
+  const backendPackageJson = require(`${rootProjectDir}/server/package.json`);
+  const frontendPackageJson = require(`${rootProjectDir}/client/package.json`);
+  const rootPackageJson = require(`${rootProjectDir}/package.json`);
 
-app.get('/versions', (req, res) => {
-  console.log('Request for versions');
-  res.send({
-    backendVersion: backendPackageJson.version,
-    frontendVersion: frontendPackageJson.version,
-    rootVersion: rootPackageJson.version,
-    coreVersion: backendPackageJson.dependencies['travis-builds-reporter-core'],
-    utilsVersion: frontendPackageJson.dependencies['travis-builds-reporter-utils'] || 'latest',
+  function fetchBuilds(requestBody, response) {
+    let { repository } = requestBody;
+    if (repository) {
+      repository = repository.trim();
+      fetcher.fetch(repository, client.create(axios))
+        .then((builds) => {
+          console.log('builds fetched');
+          response.json(builds);
+        })
+        .catch((error) => {
+          console.error(error);
+          response.status(500).send('Wrong repository or something else');
+        });
+    } else {
+      console.warn('Missing repository info');
+      response.status(500).send('Missing repository info.');
+    }
+  }
+
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.post('/builds', (req, res) => {
+    console.log('Request:', req.body, req.params);
+    fetchBuilds(req.body, res);
   });
-});
+
+  app.get('/versions', (req, res) => {
+    console.log('Request for versions');
+    res.send({
+      backendVersion: backendPackageJson.version,
+      frontendVersion: frontendPackageJson.version,
+      rootVersion: rootPackageJson.version,
+      coreVersion: backendPackageJson.dependencies['travis-builds-reporter-core'],
+      utilsVersion: frontendPackageJson.dependencies['travis-builds-reporter-utils'] || 'latest',
+    });
+  });
 
 // Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '../../client/build')), {
-    etag: false,
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(rootProjectDir, 'client/build')));
+  }
+
+  app.set('port', (process.env.PORT || 3001));
+
+  app.listen(app.get('port'), () => {
+    console.log(`Listening on ${app.get('port')}`);
   });
 }
 
-app.set('port', (process.env.PORT || 3001));
-
-app.listen(app.get('port'), () => {
-  console.log(`Listening on ${app.get('port')}`);
-});
+pathFinder(__dirname);
